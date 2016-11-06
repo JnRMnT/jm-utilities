38b7a5ee12d9244311350c07a61787ff0bacbbf4
Ozan KANIK <ozankanik@hotmail.com>
2016-11-06 17:06:59 +0300

    IsEmpty function is added & Wrapped from Lodash

--- JMUtilities/app.js
+++ JMUtilities/app.js
@@ -1,4 +1,8 @@
+var _ = require("lodash");
 module.exports.IsDefined = function (obj) {
     return obj != null && obj != undefined;
+};
+module.exports.IsEmpty = function (obj) {
+    return !module.exports.IsDefined(obj) || _.IsEmpty(obj);
 };
 //# sourceMappingURL=app.js.map
--- JMUtilities/app.ts
+++ JMUtilities/app.ts
@@ -1,3 +1,9 @@
-﻿module.exports.IsDefined = (obj: any): boolean => {
+﻿import _ = require("lodash");
+
+module.exports.IsDefined = (obj: any): boolean => {
     return obj != null && obj != undefined;
 };
+
+module.exports.IsEmpty = (obj: any): boolean => {
+    return !module.exports.IsDefined(obj) || _.IsEmpty(obj);
+}
